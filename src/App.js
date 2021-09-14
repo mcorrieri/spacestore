07c6eec15d97d57b888c64308e1325a34b58acc6
Michael <Mlcorrieri@gmail.com>
2021-09-14 14:28:17 -0400

    fetching apod data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import RoverList from "./RoverList";
 
 function App() {
-  const [spaceArr, setSpaceArr] = useState([]);
   return (
     <div className="App">
       <Header />
